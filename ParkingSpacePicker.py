75a79e3353e9daf86dc18f5fe95e2e127a029022
by-lucas <lukasmulekezika2@gmail.com>
2022-07-13 20:36:24 -0300

    Salvar dados

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -1,26 +1,31 @@
+# INFORMACOES
+# Este código voce pode mapear os locals dos do estacionamento e
+# salvar no arquvivo CarParkPos para que o código no arquivo main possa ler
 import cv2
 import pickle
 
 width, height = 107, 48
 
 try:
-    with open('CarParkPos', 'rb') as f:
+    with open('arquivos/CarParkPos', 'rb') as f:
         posList = pickle.load(f)
 except:
     posList = []
 
-
 def mouseClick(events,x,y,flags,params):
     if events == cv2.EVENT_LBUTTONDOWN:
-        posList.append((x,y))
+        posList.append((x, y))
     if events == cv2.EVENT_RBUTTONDOWN:
         for i, pos in enumerate(posList):
-            x1,y1 = pos
-            if x1 < x < x1 + width and y1 < y< y1 + height:
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
                 posList.pop(i)
+    
+    with open('arquivos/carParkPos', 'wb') as f:
+        pickle.dump(posList, f)
 
 while True:
-    img = cv2.imread('carParkImg.png')
+    img = cv2.imread('media/carParkImg.png')
 
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255,0,255),2)
@@ -28,3 +33,6 @@ while True:
     cv2.imshow("image", img)
     cv2.setMouseCallback("image", mouseClick)
     cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
--- main.py
+++ main.py
@@ -4,27 +4,22 @@ import cvzone
 import numpy as np
 
 # Video feed
-cap = cv2.VideoCapture('carPark.mp4')
+cap = cv2.VideoCapture('media/carPark.mp4')
 
-with open('CarParkPos', 'rb') as f:
+with open('arquivos/CarParkPos', 'rb') as f:
     posList = pickle.load(f)
 
 width, height = 107, 48
 
 def checkParkingSpace(imgPro):
-
     spaceCounter = 0
 
     for pos in posList:
         x, y = pos
 
         imgCrop = imgPro[y:y + height, x:x + width]
-        cv2.imshow(str(x * y), imgCrop)
+        #cv2.imshow(str(x * y), imgCrop)
         count = cv2.countNonZero(imgCrop)
-
-        # Espaço de cada carro
-        cvzone.putTextRect(img,str(count),(x,y+height-3), scale= 1.5, 
-                            thickness=1, offset=0, colorR=(0,0,255))
 
         if count < 900:
             color = (0,255,0)
@@ -34,9 +29,13 @@ def checkParkingSpace(imgPro):
             color = (0,0,255)
             thickness = 2
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+        # Espaço de cada carro
+        cvzone.putTextRect(img,str(count),(x,y+height-3), scale= 1.5, 
+                            thickness=1, offset=0, colorR=color)
+                            
     # Espaço de cada carro
-    cvzone.putTextRect(img,str(spaceCounter),(100, 50), scale= 5, 
-                        thickness=5, offset=20)
+    cvzone.putTextRect(img, f'Vagas: {spaceCounter} / {len(posList)}',(100, 50), scale= 3, 
+                        thickness=3, offset=20, colorR=(0,200,0))
 
 while True:
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
@@ -57,3 +56,6 @@ while True:
     #cv2.imshow("imageBlur", imgBlur)
     #cv2.imshow("imageThres", imgThreshold)
     cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
